3b96c8abb6b6a2d69d5a91f1594894dba6580f96
hbh112233abc <hbh112233abc@163.com>
2020-04-02 09:48:23 +0800

    增加资源操作

--- src/test.php
+++ src/test.php
@@ -1,5 +1,20 @@
 <?php
-require __DIR__.'/helper.php';
 
-$res = ue_view();
-var_dump($res);
+function test_helper(){
+    require __DIR__.'/helper.php';
+    
+    $res = ue_view();
+    var_dump($res);
+}
+
+function test_fileutil(){
+    require __DIR__.'/util/FileUtil.php';
+    $res = \bingher\ueditor\util\FileUtil::isDirEmpty('./util');
+    var_dump($res);
+    $res = \bingher\ueditor\util\FileUtil::isDirEmpty('./util11');
+    var_dump($res);
+    $res = \bingher\ueditor\util\FileUtil::isDirEmpty('./blank');
+    var_dump($res);
+}
+
+test_helper();
---
+++ src/util/Recource.php
@@ -0,0 +1,47 @@
+<?php
+namespace bingher\ueditor\util;
+
+/**
+ * 扩展安装更新及卸载的资源文件操作
+ * 1. 安装更新时,复制assets文件到/public/static/bingher/ueditor/
+ * 2. 卸载时,删除/public/static/bingher/ueditor/,如果/public/static/bingher为空也一并删除了
+ */
+class Recource
+{
+    /**
+     * 安装动作
+     *
+     * @return boolean
+     */
+    static public function install()
+    {
+        $assets = __DIR__.'/../../assets/';
+        $static = root_path().'/public/bingher/ueditor/';
+        $res = FileUtil::copyDir($assets,$static,true);
+        if(!$res){
+            throw new \Exception('资源复制失败');
+        }
+        return $res;
+    }
+
+    /**
+     * 卸载操作
+     *
+     * @return boolean
+     */
+    static public function uninstall()
+    {
+        $parentDir = root_path().'/public/bingher/';
+        $ueditorDir = $parentDir.'/ueditor/';
+        $res = FileUtil::unlinkDir($ueditorDir);
+        if($res){
+            if(FileUtil::isDirEmpty($parentDir)){
+                $res = FileUtil::unlinkDir($parentDir);
+            }
+        }
+        if(!$res){
+            throw new \Exception('资源删除失败,请手动删除'.$ueditorDir);
+        }        
+        return $res;
+    }
+}
