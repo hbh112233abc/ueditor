9bf74be3ee61bb6ecc42b4626a58e5c4f236cc38
hbh112233abc <hbh112233abc@163.com>
2020-04-05 08:49:06 +0800

    增加资源检查

--- src/UeditorService.php
+++ src/UeditorService.php
@@ -12,11 +12,15 @@ class UeditorService extends Service
     public function boot(Route $route)
     {
         $route->rule('ueditor/index', "\\bingher\\ueditor\\controller\\Ueditor@index");
+
         $route->rule('ueditor/setting/index', "\\bingher\\ueditor\\controller\\Setting@index");
         $route->rule('ueditor/setting/save', "\\bingher\\ueditor\\controller\\Setting@save");
         $route->rule('ueditor/setting/upload_imgage', "\\bingher\\ueditor\\controller\\Setting@uploadImage");
+
         $route->rule('ueditor/demo/view', "\\bingher\\ueditor\\controller\\Demo@view");
         $route->rule('ueditor/demo/setting', "\\bingher\\ueditor\\controller\\Demo@setting");
+
+        Recource::install(false); //检查资源文件
 
         $this->commands(['ueditor:publish' => Publish::class]);
     }
