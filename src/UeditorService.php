a2bfc74bdf98c0a87cf283650d3aa593a2e9eb8f
hbh112233abc <hbh112233abc@163.com>
2020-10-14 13:58:55 +0800

    路由中间件引入session中间件

--- src/UeditorService.php
+++ src/UeditorService.php
@@ -2,20 +2,24 @@
 
 namespace bingher\ueditor;
 
+use bingher\ueditor\command\Publish;
+use bingher\ueditor\util\Recource;
 use think\Route;
 use think\Service;
-use bingher\ueditor\util\Recource;
-use bingher\ueditor\command\Publish;
 
 class UeditorService extends Service
 {
     public function boot(Route $route)
     {
-        $route->rule('ueditor/index', "\\bingher\\ueditor\\controller\\Ueditor@index");
+        $route->rule('ueditor/index', "\\bingher\\ueditor\\controller\\Ueditor@index")
+            ->middleware(\think\middleware\SessionInit::class);
 
-        $route->rule('ueditor/setting/index', "\\bingher\\ueditor\\controller\\Setting@index");
-        $route->rule('ueditor/setting/save', "\\bingher\\ueditor\\controller\\Setting@save");
-        $route->rule('ueditor/setting/upload_imgage', "\\bingher\\ueditor\\controller\\Setting@uploadImage");
+        $route->rule('ueditor/setting/index', "\\bingher\\ueditor\\controller\\Setting@index")
+            ->middleware(\think\middleware\SessionInit::class);
+        $route->rule('ueditor/setting/save', "\\bingher\\ueditor\\controller\\Setting@save")
+            ->middleware(\think\middleware\SessionInit::class);
+        $route->rule('ueditor/setting/upload_imgage', "\\bingher\\ueditor\\controller\\Setting@uploadImage")
+            ->middleware(\think\middleware\SessionInit::class);
 
         $route->rule('ueditor/demo/view', "\\bingher\\ueditor\\controller\\Demo@view");
         $route->rule('ueditor/demo/setting', "\\bingher\\ueditor\\controller\\Demo@setting");
